e2aaf00b639d6eb2701e8ceb80b6145f645f64f2
gutsy47 <bestcom5055@gmail.com>
2024-05-02 20:44:47 +0300

    Feat: Datasets upload

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+import pandas as pd
+
+
 if __name__ == '__main__':
-    print("Hello, World!")
+    df = pd.read_csv("negative.csv")
+    df2 = pd.read_csv("positive.csv")
+    print(df.iloc[:, 3])
+    print(df2.iloc[:, 3])
